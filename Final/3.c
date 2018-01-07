e620be9fffcf53bdb9bea37378f958b4dae11a84
VitozzzZ <zealucius@gmail.com>
2018-01-07 11:47:12 +0800

    Create 3.c

---
+++ Final/3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mount.h>
+#include <unistd.h>
+
+int main(){
+    //系统自动挂载
+    umount("/zsq/media/RHAPSODY");
+    if (mount("/dev/sdb", "/mnt/USB/", "vfat", MS_NOATIME, NULL)) {
+        if (errno == EBUSY) {
+            printf("Mountpoint busy");
+        }
+        else {
+            printf("Mount error: %s", strerror(errno));
+        }
+        return -1;
+    }
+    else {
+        printf("Mount successful");
+        printf("Press <return> to unmount the usb disk:");
+        getchar();
+        
+        umount("/mnt/USB/);
+    }
+    return 0;
+   
+}
