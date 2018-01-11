b13e72405172867a06e193b70b1c052c2f221182
VitozzzZ <zealucius@gmail.com>
2018-01-12 02:02:40 +0800

    Update exercise3.c

--- Final/exercise3.c
+++ Final/exercise3.c
@@ -3,49 +3,52 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
-using namespace std;                                                                             
+using namespace std;
 int readFileList(char *basePath)
 {
-       DIR *dir;
-       struct dirent *ptr;
-       char base[1000];
-       if((dir=opendir(basePath))==NULL)
+    DIR *dir;
+    struct dirent *ptr;
+    char base[1000];
+    if((dir=opendir(basePath))==NULL)
+    {
+        cout<<"open dir error!"<<endl;
+    }
+    while((ptr=readdir(dir))!=NULL)
+    {
+        if(strcmp(ptr->d_name,".")==0||strcmp(ptr->d_name,"..")==0)
+        {continue;}
+        else
         {
-         cout<<"open dir error!"<<endl;
-        }
-       while((ptr=readdir(dir))!=NULL)
-        {
-          if(strcmp(ptr->d_name,".")==0||strcmp(ptr->d_name,"..")==0)
-          {continue;}
-           else
-            {
             if(ptr->d_type==8)
-            {cout<<"d_name"<<basePath<<ptr->d_name<<endl;}
+            {
+                cout<<"d_name"<<basePath<<ptr->d_name<<endl;}
             else
-              { 
-             if(ptr->d_type==10)
-             {cout<<"d_name"<<basePath<<ptr->d_name<<endl;}
-            else
-               {
-              if(ptr->d_type==4)
-              {memset(base,'\0',sizeof(base));
-               strcpy(base,basePath);
-               strcat(base,"/");
-               strcat(base,ptr->d_name);
-               readFileList(base);
-                 }
-               }
-               }
+            {
+                if(ptr->d_type==10)
+                {
+                    cout<<"d_name"<<basePath<<ptr->d_name<<endl;}
+                else
+                {
+                    if(ptr->d_type==4)
+                    {
+                        memset(base,'\0',sizeof(base));
+                        strcpy(base,basePath);
+                        strcat(base,"/");
+                        strcat(base,ptr->d_name);
+                        readFileList(base);
+                    }
+                }
             }
-         }
-closedir(dir);
-return 1;
+        }
+    }
+    closedir(dir);
+    return 1;
 }
 int main()
 {
-char basePath[1000];
-cout<<"the current dir is";
-cin>>basePath;
-readFileList(basePath);
-return 0;
+    char basePath[1000];
+    cout<<"the current dir is";
+    cin>>basePath;
+    readFileList(basePath);
+    return 0;
 }
