8b7097e99a271802dc239667af5b1fe6f678df1a
VitozzzZ <zealucius@gmail.com>
2018-01-12 12:44:27 +0800

    Update FileDelete.h

--- Final/exercise2/FileDelete.h
+++ Final/exercise2/FileDelete.h
@@ -9,7 +9,7 @@ int fdelete(FILE *fp, long offset, int len){
     fpTemp = tmpfile();
     fcopy(fp, 0, offset, fpTemp, 0);  //将前offset字节的数据复制到临时文件
     fcopy(fp, offset+len, -1, fpTemp, offset);  //将offset+len之后的所有内容都复制到临时文件
-    freopen("/Users/apple/Desktop/file", "wb+", fp );  //重新打开文件
+    freopen("/home/zsq/2.txt", "wb+", fp );  //重新打开文件
     fcopy(fpTemp, 0, -1, fp, 0);
     fclose(fpTemp);
     return 0;
